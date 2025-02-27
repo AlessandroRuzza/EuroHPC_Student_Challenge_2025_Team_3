b83724239f43fb199a60aaed0d60d1616cd906bb
AlessandroRuzza <alessandroruzza22@gmail.com>
2025-02-27 21:33:42 +0000

    Fix shell according to best results

--- code/run_job.sh
+++ code/run_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=cpu
-#SBATCH --nodes=2
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --time=02:46:40
 
 module load mpi4py
 module load SciPy-bundle
 
-srun --output=$2/${1:0:-4}.stdout python MPI/mpi.py ../instances/$1 $2 --cpusPerTask 16
+srun --output=$2/${1:0:-4}.stdout python MPI/mpi.py ../instances/$1 $2 --cpusPerTask 32
